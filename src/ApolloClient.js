8f906911f2c44fdf218a18583539ddf75e55324b
bassem <bassem.gehad95@yahoo.com>
2025-01-04 15:34:41 +0200

    php-task

--- src/ApolloClient.js
+++ src/ApolloClient.js
@@ -1,7 +1,8 @@
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 
 const client = new ApolloClient({
-  uri: 'https://f22c-197-32-64-10.ngrok-free.app', // Replace with your GraphQL endpoint
+  // uri: 'https://f22c-197-32-64-10.ngrok-free.app', // Replace with your GraphQL endpoint
+  uri: 'http://task-php.ct.ws/', // Replace with your GraphQL endpoint
   cache: new InMemoryCache(),
 });
 
